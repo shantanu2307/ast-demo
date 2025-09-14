cda1aabc0a52d77261501f2e18c632fdca9bfb8a
Shantanu Goyal <shantanugoyal23@gmail.com>
2025-09-14 11:19:26 +0530

    chore: generate patch file

--- utils/diff.ts
+++ utils/diff.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 import { ROOT_PATH, PATCH_FILE_PATH } from '../constants';
 
 export default function diff(filePath: string, oldFile: string, newFile: string): string {
-  if (oldFile === newFile || process.env.TEST === 'true') return newFile;
+  if (oldFile === newFile) return newFile;
   const diffs = createPatch(filePath, oldFile, newFile);
   fs.appendFileSync(PATCH_FILE_PATH, `${diffs.replaceAll(ROOT_PATH || /^[^/]*/, '@')}\n\n\n`);
   return newFile;
